e4c889e4ba576532160d79e0c3c5131bacc69d0c
Joe Harrison <email@joeharrison.com>
2015-02-10 10:35:54 -0500

    added getting started page

---
+++ MVCGridExample/Controllers/GettingStartedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGrid.Web.Controllers
+{
+    public class GettingStartedController : Controller
+    {
+        // GET: GettingStarted
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
