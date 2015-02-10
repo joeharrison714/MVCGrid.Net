bf1304546c13681f09370317c0cfb817cc51c335
Joe Harrison <email@joeharrison.com>
2015-02-10 14:31:29 -0500

    added download page

---
+++ MVCGridExample/Controllers/DownloadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGrid.Web.Controllers
+{
+    public class DownloadController : Controller
+    {
+        // GET: Download
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
