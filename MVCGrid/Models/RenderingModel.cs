9f19948963c0eb1fca5231cc9b1113ddda685151
Joe Harrison <email@joeharrison.com>
2015-02-10 22:00:07 -0500

    experimenting with razor rendering engine

---
+++ MVCGrid/Models/RenderingModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCGrid.Models
+{
+    public class RenderingModel
+    {
+        public RenderingModel()
+        {
+            Columns = new List<Column>();
+            Rows = new List<Row>();
+        }
+        public List<Column> Columns { get; set; }
+        public List<Row> Rows { get; set; }
+    }
+
+    public class Cell
+    {
+        public string CalculatedCssClass { get; set; }
+        public string Html { get; set; }
+        public string PlainText { get; set; }
+    }
+
+    public class Row
+    {
+        public Row()
+        {
+            Cells = new Dictionary<string, Cell>();
+        }
+        public string CalculatedCssClass { get; set; }
+        public Dictionary<string, Cell> Cells { get; set; }
+    }
+
+    public class Column
+    {
+        public string Name { get; set; }
+        public string Onclick { get; set; }
+        public string HeaderText { get; set; }
+        public SortDirection? SortIconDirection { get; set; }
+    }
+}
---
+++ MVCGrid/Rendering/RazorRenderingEngine.cs
@@ -0,0 +1,122 @@
+﻿using MVCGrid.Interfaces;
+using MVCGrid.Models;
+using MVCGrid.Utility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MVCGrid.Rendering
+{
+    public class RazorRenderingEngine : IMVCGridRenderingEngine
+    {
+        public bool AllowsPaging
+        {
+            get { return true; }
+        }
+
+        public void PrepareResponse(System.Web.HttpResponse response)
+        {
+        }
+
+        public void Render(Models.GridData data, Models.GridContext gridContext, System.IO.Stream outputStream)
+        {
+            RenderingModel model = new RenderingModel();
+
+            PrepColumns(gridContext, model);
+            PrepRows(data, gridContext, model);
+
+            string filename = HttpContext.Current.Server.MapPath("~/App_Data/Template.cshtml");
+            string templateContents;
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                templateContents = sr.ReadToEnd();
+            }
+
+
+        }
+
+        private static void PrepRows(Models.GridData data, Models.GridContext gridContext, RenderingModel model)
+        {
+            foreach (var item in data.Rows)
+            {
+                Row renderingRow = new Row();
+                model.Rows.Add(renderingRow);
+
+                if (!String.IsNullOrWhiteSpace(item.RowCssClass))
+                {
+                    renderingRow.CalculatedCssClass = item.RowCssClass;
+                }
+
+                foreach (var col in gridContext.GetVisibleColumns())
+                {
+                    string val = "";
+
+                    if (item.Values.ContainsKey(col.ColumnName))
+                    {
+                        val = item.Values[col.ColumnName];
+                    }
+
+                    Cell renderingCell = new Cell();
+                    renderingRow.Cells.Add(col.ColumnName, renderingCell);
+                    if (item.CellCssClasses.ContainsKey(col.ColumnName))
+                    {
+                        string cellCss = item.CellCssClasses[col.ColumnName];
+                        if (!String.IsNullOrWhiteSpace(cellCss))
+                        {
+                            renderingCell.CalculatedCssClass = cellCss;
+                        }
+                    }
+
+                    if (col.HtmlEncode)
+                    {
+                        renderingCell.Html = HttpUtility.HtmlEncode(val);
+                    }
+                    else
+                    {
+                        renderingCell.Html = val;
+                    }
+                }
+            }
+        }
+
+        private static void PrepColumns(Models.GridContext gridContext, RenderingModel model)
+        {
+            foreach (var col in gridContext.GetVisibleColumns())
+            {
+                Column renderingColumn = new Column();
+                model.Columns.Add(renderingColumn);
+                renderingColumn.Name = col.ColumnName;
+                renderingColumn.HeaderText = col.HeaderText;
+
+                if (gridContext.GridDefinition.Sorting && col.EnableSorting)
+                {
+                    SortDirection linkDirection = SortDirection.Asc;
+                    SortDirection iconDirection = SortDirection.Unspecified;
+
+                    if (gridContext.QueryOptions.SortColumn == col.ColumnName && gridContext.QueryOptions.SortDirection == SortDirection.Asc)
+                    {
+                        iconDirection = SortDirection.Asc;
+                        linkDirection = SortDirection.Dsc;
+                    }
+                    else if (gridContext.QueryOptions.SortColumn == col.ColumnName && gridContext.QueryOptions.SortDirection == SortDirection.Dsc)
+                    {
+                        iconDirection = SortDirection.Dsc;
+                        linkDirection = SortDirection.Asc;
+                    }
+                    else
+                    {
+                        iconDirection = SortDirection.Unspecified;
+                        linkDirection = SortDirection.Dsc;
+                    }
+
+                    renderingColumn.Onclick = HtmlUtility.MakeSortLink(gridContext.GridName, col.ColumnName, linkDirection);
+                    renderingColumn.SortIconDirection = iconDirection;
+                }
+            }
+        }
+    }
+}
