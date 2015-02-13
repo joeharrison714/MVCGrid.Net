e73b77f8555a765941748e05a458dd6c98da4402
Joe Harrison <email@joeharrison.com>
2015-02-13 10:07:16 -0500

    testing using controller for grid callback

---
+++ MVCGrid/Web/MVCGridController.cs
@@ -0,0 +1,31 @@
+﻿using MVCGrid.Engine;
+using MVCGrid.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MVCGrid.Web
+{
+    public class MVCGridController : Controller
+    {
+        public ActionResult Index()
+        {
+            var context = System.Web.HttpContext.Current;
+
+            string gridName = context.Request["Name"];
+
+            var grid = MVCGridDefinitionTable.GetDefinitionInterface(gridName);
+
+            var options = QueryStringParser.ParseOptions(grid, context.Request);
+
+            var gridContext = GridContextUtility.Create(context, gridName, grid, options);
+
+            GridEngine engine = new GridEngine();
+            var renderingModel = engine.GenerateModel(gridContext);
+
+            return View(renderingModel);
+        }
+    }
+}
--- MVCGridExample/App_Start/RouteConfig.cs
+++ MVCGridExample/App_Start/RouteConfig.cs
@@ -18,6 +18,13 @@ namespace MVCGridExample
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                "MVCGrid",
+                "mvcgrid/{action}",
+                new { controller = "MVCGrid" },
+                new[] { "MVCGrid.Web" }
+            );
         }
     }
 }
