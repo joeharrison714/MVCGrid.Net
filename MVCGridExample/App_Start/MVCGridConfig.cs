1e586302ec4b4f3c2a834da6b669bda52cad85d2
Joe Harrison <email@joeharrison.com>
2015-02-14 21:08:11 -0500

    Added usage example

--- MVCGridExample/App_Start/MVCGridConfig.cs
+++ MVCGridExample/App_Start/MVCGridConfig.cs
@@ -727,6 +727,30 @@ namespace MVCGridExample
                 .WithRetrieveDataMethod(docsLoadData)
             );
 
+            MVCGridDefinitionTable.Add("UsageExample", new MVCGridBuilder<YourModelItem>()
+                .AddColumns(cols =>
+                {
+                    // Add your columns here
+                    cols.Add().WithColumnName("UniqueColumnName")
+                        .WithHeaderText("Any Header")
+                        .WithValueExpression((i, c) => i.YourProperty); // use the Value Expression to return the cell text for this column
+                })
+                .WithRetrieveDataMethod((options) =>
+                {
+                    // Query your data here. Obey Ordering, paging and filtering paramters given in the options.
+                    // Use Entity Framwork, a module from your IoC Container, or any other method.
+                    // Return QueryResult object containing IEnumerable<YouModelItem>
+
+                    return new QueryResult<YourModelItem>()
+                    {
+                        Items = new List<YourModelItem>(),
+                        TotalRecords = 0 // if paging is enabled, return the total number of records of all pages
+                    };
+
+                })
+            );
+
+
 
             //MVCGridDefinitionTable.Add DO NOT DELETE - Needed for demo code parsing
         }
---
+++ MVCGridExample/Models/YourModelItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGrid.Web.Models
+{
+    public class YourModelItem
+    {
+        public string YourProperty { get; set; }
+    }
+}
