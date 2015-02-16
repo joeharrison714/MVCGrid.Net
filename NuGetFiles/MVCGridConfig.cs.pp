e7e29e5696049765f9bebf6bcd4b0331b6c23dc7
joeharrison714 <bitbucket@joeharrison.com>
2015-02-15 23:28:03 -0500

    enhancing nuget package

--- NuGetFiles/MVCGridConfig.cs.pp
+++ NuGetFiles/MVCGridConfig.cs.pp
@@ -15,6 +15,30 @@ namespace $rootnamespace$.App_Start
     {
         public static void RegisterGrids()
         {
-		}    
+			/*
+            MVCGridDefinitionTable.Add("UsageExample", new MVCGridBuilder<YourModelItem>()
+                .AddColumns(cols =>
+                {
+                    // Add your columns here
+                    cols.Add().WithColumnName("UniqueColumnName")
+                        .WithHeaderText("Any Header")
+                        .WithValueExpression((i, c) => i.YourProperty); // use the Value Expression to return the cell text for this column
+                })
+                .WithRetrieveDataMethod((context) =>
+                {
+                    // Query your data here. Obey Ordering, paging and filtering paramters given in the context.QueryOptions.
+                    // Use Entity Framwork, a module from your IoC Container, or any other method.
+                    // Return QueryResult object containing IEnumerable<YouModelItem>
+
+                    return new QueryResult<YourModelItem>()
+                    {
+                        Items = new List<YourModelItem>(),
+                        TotalRecords = 0 // if paging is enabled, return the total number of records of all pages
+                    };
+
+                })
+            );
+            */
+		}
     }
 }
