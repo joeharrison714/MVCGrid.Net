c34ffed7fb4495c71a08fe650a2f2d992c05118f
Joe Harrison <email@joeharrison.com>
2015-03-23 14:23:05 -0400

    nuget installation script development

--- NuGetFiles/Install.ps1
+++ NuGetFiles/Install.ps1
@@ -1,13 +1,40 @@
  param($installPath, $toolsPath, $package, $project)
 
+ $codeFilename = "MVCGridConfig.cs"
+
 # Get the project item for the scripts folder
 try {
-    $scriptsFolderProjectItem = $project.ProjectItems.Item("App_Start")
-    $projectScriptsFolderPath = $scriptsFolderProjectItem.FileNames(1)
+    $appStartFolderProjectItem = $project.ProjectItems.Item("App_Start")
+    $appStartFolderPath = $appStartFolderProjectItem.FileNames(1)
 
-    Write-Host "AppStart: " + $projectScriptsFolderPath
+    Write-Host "AppStart: ($appStartFolderPath)"
 }
 catch {
     # No Scripts folder
-    Write-Host "No scripts folder found"
+    Write-Host "No App_Data folder found"
+    exit
 }
+
+try{
+    If (Test-Path $appStartFolderPath){
+        Write-Host "File already exists ($appStartFolderPath)"
+        exit
+    }
+}
+catch{
+    Write-Host "Error checking for file"
+    exit
+}
+
+try {
+    $sourcePath = Join-Path $toolsPath $codeFilename
+    Write-Host "sourcePath: ($sourcePath)"
+    $appStartFolderProjectItem.ProjectItems.AddFromTemplate($sourcePath)
+}
+catch {
+    # No Scripts folder
+    Write-Host "Error adding file"
+    exit
+}
+
+#$toolsPath
---
+++ NuGetFiles/MVCGridConfig.cs
@@ -0,0 +1,44 @@
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.MVCGridConfig), "RegisterGrids")]
+
+namespace $rootnamespace$.App_Start
+{
+    using System;
+    using System.Web;
+	using System.Web.Mvc;
+	using System.Linq;
+	using System.Collections.Generic;
+
+	using MVCGrid.Models;
+	using MVCGrid.Web;
+
+    public static class MVCGridConfig 
+    {
+        public static void RegisterGrids()
+        {
+			/*
+            MVCGridDefinitionTable.Add("UsageExample", new MVCGridBuilder<YourModelItem>()
+                .AddColumns(cols =>
+                {
+                    // Add your columns here
+                    cols.Add().WithColumnName("UniqueColumnName")
+                        .WithHeaderText("Any Header")
+                        .WithValueExpression((i, c) => i.YourProperty); // use the Value Expression to return the cell text for this column
+                })
+                .WithRetrieveDataMethod((context) =>
+                {
+                    // Query your data here. Obey Ordering, paging and filtering paramters given in the context.QueryOptions.
+                    // Use Entity Framwork, a module from your IoC Container, or any other method.
+                    // Return QueryResult object containing IEnumerable<YouModelItem>
+
+                    return new QueryResult<YourModelItem>()
+                    {
+                        Items = new List<YourModelItem>(),
+                        TotalRecords = 0 // if paging is enabled, return the total number of records of all pages
+                    };
+
+                })
+            );
+            */
+		}
+    }
+}
