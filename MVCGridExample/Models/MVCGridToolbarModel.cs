bb61db429b69834cc7dad5e4814271dacdff8567
joeharrison714 <bitbucket@joeharrison.com>
2015-03-24 17:12:56 -0400

    added model for toolbar and switched homepage to use toolbar partial view

---
+++ MVCGridExample/Models/MVCGridToolbarModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGrid.Web.Models
+{
+    public class MVCGridToolbarModel
+    {
+        public MVCGridToolbarModel()
+        {
+
+        }
+
+        public MVCGridToolbarModel(string gridName)
+        {
+            MVCGridName = gridName;
+        }
+
+        public string MVCGridName { get; set; }
+        public bool PageSize { get; set; }
+        public bool ColumnVisibility { get; set; }
+        public bool Export { get; set; }
+        public bool GlobalSearch { get; set; }
+    }
+}
